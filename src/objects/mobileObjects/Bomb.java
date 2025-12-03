a01b3f1d3682136c271b61ca52698469d22ad525
JoaoCabanas <jtcml@iscte-iul.pt>
2025-12-03 18:23:02 +0000

    bomba

--- src/objects/mobileObjects/Bomb.java
+++ src/objects/mobileObjects/Bomb.java
@@ -1,11 +1,16 @@
 package objects.mobileObjects;
 
+import java.util.List;
+
 import objects.*;
 import pt.iscte.poo.game.Room;
 import pt.iscte.poo.utils.Direction;
 import pt.iscte.poo.utils.Point2D;
 
 public class Bomb extends MobileObject {
+
+	private boolean isFalling = false;
+
     public Bomb(Point2D p) {
 		super(p);
 	}
@@ -14,13 +19,36 @@ public class Bomb extends MobileObject {
 	public String getName() {
 		return "bomb";
 	}
+	public void tickAction(Room room){
+		boolean moved = move(null,Direction.DOWN,room);
 
-
-	@Override
-	public boolean interact(GameObject object, Direction direction, Room room) {
-		if(object instanceof GameCharacter){
-			((GameCharacter)object).kill(room);
+		if(moved){
+			isFalling = true;
+		} else {
+			if(isFalling){
+			explode(room);
+			}
+			isFalling = false;
 		}
-		return false;
 	}
+			
+	private void explode(Room room) {
+		List<Point2D> area = getPosition().getNeighbourhoodPoints();
+
+		for(Point2D p : area){
+			List<GameObject> objects = room.getObjectsAt(p);
+
+				for(int i = objects.size() - 1; i >= 0; i--){
+					GameObject obj = objects.get(i);
+					if(obj instanceof GameCharacter){
+						((GameCharacter) obj).kill(room);
+					}
+					else {
+						room.removeObject(obj);
+					}
+				}
+			}
+		room.removeObject(this);
+		}
+
 }
