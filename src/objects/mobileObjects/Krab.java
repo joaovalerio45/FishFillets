1ff8574846afe1322624d866d95a8a79aae54a0e
JoaoCabanas <jtcml@iscte-iul.pt>
2025-12-07 22:38:13 +0000

    a

--- src/objects/mobileObjects/Krab.java
+++ src/objects/mobileObjects/Krab.java
@@ -30,6 +30,8 @@ public class Krab extends MobileObject {
         return true;
     }
 
+
+    //se interagir com um peixe grande, morre(é removido), e se interagir com um peixe pequeno mata-o
     @Override
     public boolean interact(GameObject object, Direction direction, Room room) {
         if (object instanceof BigFish) {
@@ -43,10 +45,13 @@ public class Krab extends MobileObject {
         return false;
     }
 
+
     @Override
     public void fishMoveAction(Room room) {
         Random rand = new Random();
-		int d = rand.nextInt(2); 
+
+        //cria um numero aleatorio de 0 a 1, se for 0 o carangueijo vai para a esquerda e se for 1 vai para a direita
+		int d = rand.nextInt(2);
         Direction direction;
         if(d == 0){
             direction = Direction.LEFT;
